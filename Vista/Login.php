79a8ef5c4124c7dacb46b02ce7f9fc97bc0f0f16
AlbertoR17 <alberto9406@gmail.com>
2018-05-16 09:54:32 -0700

    Se subieron archivos del template
    css,js,pluggins y parte del login

---
+++ Vista/Login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+
+<!-- Mirrored from webapplayers.com/inspinia_admin-v2.7.1/login_two_columns.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 04 Oct 2017 15:26:54 GMT -->
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Inmobiliaria</title>
+
+    <link href="../Content/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../Contentfont-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="../Content/css/animate.css" rel="stylesheet">
+    <link href="../Content/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="loginColumns animated fadeInDown">
+        <div class="row">
+
+            <div class="col-md-6">
+                <h2 class="font-bold">Inmobiliaria</h2>
+
+                <p>
+                    <h3>Espacio a tu medida</h3>
+                    
+                </p>
+
+                
+
+            </div>
+            <div class="col-md-6">
+                <div class="ibox-content">
+                    <form class="m-t" role="form" action="http://webapplayers.com/inspinia_admin-v2.7.1/index.html">
+                        <center><img src="../Content/img/logo.png" alt=""></center>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Correo" required="">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Contraseña" required="">
+                        </div>
+                        <button type="submit" class="btn btn-primary block full-width m-b">Entrar</button>
+
+                        
+                        <!-- <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a> -->
+                    </form>
+                    <p class="m-t">
+                        <small>Desarrollo de Software con Enfoque en el Negocio </small>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-md-6">
+              
+            </div>
+            <div class="col-md-6 text-right">
+               <small>  </small>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+<!-- Mirrored from webapplayers.com/inspinia_admin-v2.7.1/login_two_columns.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 04 Oct 2017 15:26:54 GMT -->
+</html>
